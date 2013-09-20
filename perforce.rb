f23c215b5c001b78f75649da8859c204a0b6cfae
Dusty Leary <dleary@gmail.com>
2013-09-20 11:14:12 -0700

    Update perforce.rb

--- perforce.rb
+++ perforce.rb
@@ -6,7 +6,7 @@ class Perforce < Formula
   if MacOS.prefer_64_bit?
     url 'http://filehost.perforce.com/perforce/r13.1/bin.darwin90x86_64/p4'
     version '2013.1.659207-x86_64'
-    sha1 '1963ba02c5b3b53135ba42c8c8791ff7'
+    sha1 '77737480a93bb68d4874c4d242e42680cbd045e5'
   else
     url 'http://filehost.perforce.com/perforce/r13.1/bin.darwin90x86/p4'
     version '2013.1.659207-x86'
