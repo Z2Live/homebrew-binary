e0356ea0004735b3d5a19fc3c2ccf71f19f6bb31
tychoc <tychoc@users.noreply.github.com>
2017-08-11 10:52:12 -0700

    Add files via upload
    updates perforce.rb to 2017.1 and SHA256

--- perforce.rb
+++ perforce.rb
@@ -4,13 +4,13 @@ class Perforce < Formula
   homepage 'http://www.perforce.com/'
 
   if MacOS.prefer_64_bit?
-    url 'http://filehost.perforce.com/perforce/r13.1/bin.darwin90x86_64/p4'
-    version '2013.1.659207-x86_64'
-    sha1 'a5d1f6399131fd9973990eb3e0f7d5ec73975613'
+    url 'http://filehost.perforce.com/perforce/r17.1/bin.darwin90x86_64/p4'
+    version '2017.1-x86_64'
+    sha256 '1a17923f8b0092798ed66a0ae4ca71fc27ac67dfb75156078aab3af0cc7ec455'
   else
-    url 'http://filehost.perforce.com/perforce/r13.1/bin.darwin90x86/p4'
-    version '2013.1.659207-x86'
-    sha1 '1963ba02c5b3b53135ba42c8c8791ff7'
+    url 'http://filehost.perforce.com/perforce/r17.1/bin.darwin90x86/p4'
+    version '2017.1-x86'
+    sha256 '7c2fd4288b48b52b04dd5a63f87e95346210ada765047e851d8bf96215a31326'
   end
 
   def install
